839716d49f6e26b4c9abe6dabb4bad3150317df2
Andres <141367266+Anfema16@users.noreply.github.com>
2023-11-16 18:44:54 -0500

    Add files via upload

--- exam/SimuladorBicicleta/src/SimuladorBicicleta.java
+++ exam/SimuladorBicicleta/src/SimuladorBicicleta.java
@@ -3,81 +3,132 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/*Autor: Andres Felipe Martinez Orozco*/
+//Autor: Andres Felipe Martinez Orozco
 
 public class SimuladorBicicleta extends JFrame {
 
-    private JButton pedalIzquierdo, pedalDerecho;
-    private JLabel indicador1, indicador2, temporizadorLabel;
+    private JButton pedalIzquierdo, pedalDerecho, automaticoButton, manualButton;
+    private JLabel indicador1, indicador2, temporizadorLabel, contadorPedalesIzquierdo, contadorPedalesDerecho;
     private Timer tiempo;
     private int contadorPedalIzquierdo, contadorPedalDerecho, segundosTranscurridos;
     private double velocidad;
     private int calorias;
+    private boolean automaticoActivado;
 
     public SimuladorBicicleta() {
-        //Titulo del programa, tamaño de la ventana, si se cierra, detiene el programa
-        setTitle("Bicicleta");
+        setTitle("Simulador de Bicicleta");
         setSize(600, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        //Botones de los Pedales
+
         pedalIzquierdo = new JButton("Izquierda");
         pedalDerecho = new JButton("Derecha");
-        
-        //Indicadores de Velocidad, Calorias y Tiempo
+        automaticoButton = new JButton("Automático");
+        manualButton = new JButton("Manual");
+
         indicador1 = new JLabel("Velocidad: 0.0 km/h");
         indicador2 = new JLabel("Calorías quemadas: 0");
         temporizadorLabel = new JLabel("Tiempo: 0 segundos");
-        
-        // Se asigna las filas y Columnas
-        setLayout(new GridLayout(4, 2));
+        contadorPedalesIzquierdo = new JLabel("Pedaleadas Izquierda: 0");
+        contadorPedalesDerecho = new JLabel("Pedaleadas Derecha: 0");
+
+        setLayout(new GridLayout(6, 2));
         add(indicador1);
         add(indicador2);
         add(pedalIzquierdo);
         add(pedalDerecho);
+        add(contadorPedalesIzquierdo);
+        add(contadorPedalesDerecho);
+        add(automaticoButton);
+        add(manualButton);
         add(temporizadorLabel);
 
-        // Temporizador con intervalo de 1000 milisegundos (1 segundo)
         tiempo = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 segundosTranscurridos++;
-                temporizadorLabel.setText("Tiempo " + segundosTranscurridos + " segundos");
+                temporizadorLabel.setText("Tiempo: " + segundosTranscurridos + " segundos");
                 indicador1.setText("Velocidad: " + velocidad + " km/h");
                 indicador2.setText("Calorías quemadas: " + calorias);
             }
         });
         tiempo.start();
-        
-        //Clase del Pedal Izquierdo
+
         pedalIzquierdo.addActionListener(new ActionListener() {
+            int clicksIzquierdo = 0;
             @Override
             public void actionPerformed(ActionEvent e) {
-                contadorPedalIzquierdo++;
-                pedalIzquierdo.setVisible(true);
-                pedalDerecho.setVisible(true);
-                simularBicicleta();
+                if (!automaticoActivado) {
+                    clicksIzquierdo++;
+                    if (clicksIzquierdo >= (int) (Math.random() * 3) + 2) {
+                        contadorPedalIzquierdo++;
+                        contadorPedalesIzquierdo.setText("Pedaleadas Izquierda: " + contadorPedalIzquierdo);
+                        simularBicicleta();
+                        clicksIzquierdo = 0;
+                    }
+                }
             }
         });
-        
-        //Clase del Pedal Derecho
+
         pedalDerecho.addActionListener(new ActionListener() {
+            int clicksDerecho = 0;
             @Override
             public void actionPerformed(ActionEvent e) {
-                contadorPedalDerecho++;
-                pedalDerecho.setVisible(true);
+                if (!automaticoActivado) {
+                    clicksDerecho++;
+                    if (clicksDerecho >= (int) (Math.random() * 3) + 2) {
+                        contadorPedalDerecho++;
+                        contadorPedalesDerecho.setText("Pedaleadas Derecha: " + contadorPedalDerecho);
+                        simularBicicleta();
+                        clicksDerecho = 0;
+                    }
+                }
+            }
+        });
+
+        automaticoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                automaticoActivado = true;
+                pedalIzquierdo.setVisible(false);
+                pedalDerecho.setVisible(false);
+                iniciarAvanceAutomatico();
+            }
+        });
+
+        manualButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                automaticoActivado = false;
                 pedalIzquierdo.setVisible(true);
-                simularBicicleta();
+                pedalDerecho.setVisible(true);
             }
         });
     }
-    
-    private void simularBicicleta() {
-        // Simular el cálculo de la velocidad (velocidad entre 10 y 30 km/h)
-        velocidad = Math.random() * 20 + 10;
 
-        // Simular el cálculo de las calorías quemadas (cada pedalada quema 5 calorías)
+    private void simularBicicleta() {
+        velocidad = Math.random() * 20 + 10;
         calorias = (contadorPedalIzquierdo + contadorPedalDerecho) * 5;
+    }
+
+    private void iniciarAvanceAutomatico() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (automaticoActivado) {
+                    try {
+                        Thread.sleep(1500); // Avance cada 1.5 segundos
+                        contadorPedalIzquierdo++;
+                        contadorPedalesIzquierdo.setText("Pedaleadas Izquierda: " + contadorPedalIzquierdo);
+                        contadorPedalDerecho++;
+                        contadorPedalesDerecho.setText("Pedaleadas Derecha: " + contadorPedalDerecho);
+                        simularBicicleta();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
     }
 
     public static void main(String[] args) {
