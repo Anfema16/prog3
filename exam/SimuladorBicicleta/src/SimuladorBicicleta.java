31b45f37aa15f1fc58a9d90a454d73c6bc06f326
Andres <141367266+Anfema16@users.noreply.github.com>
2023-11-16 15:56:28 -0500

    Add files via upload

--- exam/SimuladorBicicleta/src/SimuladorBicicleta.java
+++ exam/SimuladorBicicleta/src/SimuladorBicicleta.java
@@ -3,43 +3,42 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/*Autor: Andres Felipe Martinez Orozco*/
-
 public class SimuladorBicicleta extends JFrame {
 
     private JButton pedalIzquierdo, pedalDerecho;
     private JLabel indicador1, indicador2, temporizadorLabel;
     private Timer tiempo;
     private int contadorPedalIzquierdo, contadorPedalDerecho, segundosTranscurridos;
+    private double velocidad;
+    private int calorias;
 
     public SimuladorBicicleta() {
-        setTitle("Bicicleta");
-        setSize(400, 200);
+        setTitle("Simulador de Bicicleta");
+        setSize(600, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         pedalIzquierdo = new JButton("Izquierda");
         pedalDerecho = new JButton("Derecha");
 
-        indicador1 = new JLabel("Contador Pedal Izquierdo: 0");
-        indicador2 = new JLabel("Contador Pedal Derecho: 0");
+        indicador1 = new JLabel("Velocidad: 0 km/h");
+        indicador2 = new JLabel("Calorías quemadas: 0");
         temporizadorLabel = new JLabel("Tiempo: 0 segundos");
 
-        setLayout(new GridLayout(3, 2));
+        setLayout(new GridLayout(4, 2));
         add(indicador1);
         add(indicador2);
         add(pedalIzquierdo);
         add(pedalDerecho);
         add(temporizadorLabel);
-        
 
         // Temporizador con intervalo de 1000 milisegundos (1 segundo)
         tiempo = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 segundosTranscurridos++;
-                temporizadorLabel.setText("Tiempo: " + segundosTranscurridos + " segundos");
-                indicador1.setText("Contador Pedal Izquierdo: " + contadorPedalIzquierdo);
-                indicador2.setText("Contador Pedal Derecho: " + contadorPedalDerecho);
+                temporizadorLabel.setText("Tiempo " + segundosTranscurridos + " segundos");
+                indicador1.setText("Velocidad: " + velocidad + " km/h");
+                indicador2.setText("Calorías quemadas: " + calorias);
             }
         });
         tiempo.start();
@@ -50,6 +49,7 @@ public class SimuladorBicicleta extends JFrame {
                 contadorPedalIzquierdo++;
                 pedalIzquierdo.setVisible(true);
                 pedalDerecho.setVisible(true);
+                simularBicicleta();
             }
         });
 
@@ -59,8 +59,17 @@ public class SimuladorBicicleta extends JFrame {
                 contadorPedalDerecho++;
                 pedalDerecho.setVisible(true);
                 pedalIzquierdo.setVisible(true);
+                simularBicicleta();
             }
         });
+    }
+
+    private void simularBicicleta() {
+        // Simular el cálculo de la velocidad (por ejemplo, velocidad aleatoria entre 10 y 30 km/h)
+        velocidad = Math.random() * 20 + 10;
+
+        // Simular el cálculo de las calorías quemadas (cada pedalada quema 5 calorías)
+        calorias = (contadorPedalIzquierdo + contadorPedalDerecho) * 5;
     }
 
     public static void main(String[] args) {
