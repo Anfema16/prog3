15c5446cf0024ce75b819dc79841edc8d3356d9d
Andres <141367266+Anfema16@users.noreply.github.com>
2023-11-16 15:31:50 -0500

    Add files via upload

--- exam/SimuladorBicicleta/src/SimuladorBicicleta.java
+++ exam/SimuladorBicicleta/src/SimuladorBicicleta.java
@@ -1,14 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /*Autor: Andres Felipe Martinez Orozco*/
 
 public class SimuladorBicicleta extends JFrame {
 
     private JButton pedalIzquierdo, pedalDerecho;
-    private final JLabel indicador1;
-    private final JLabel indicador2;
+    private JLabel indicador1, indicador2, temporizadorLabel;
+    private Timer tiempo;
+    private int contadorPedalIzquierdo, contadorPedalDerecho, segundosTranscurridos;
 
     public SimuladorBicicleta() {
         setTitle("Bicicleta");
@@ -18,31 +20,55 @@ public class SimuladorBicicleta extends JFrame {
         pedalIzquierdo = new JButton("Izquierda");
         pedalDerecho = new JButton("Derecha");
 
-        indicador1 = new JLabel("Indicador 1: ");
-        indicador2 = new JLabel("Indicador 2: ");
+        indicador1 = new JLabel("Contador Pedal Izquierdo: 0");
+        indicador2 = new JLabel("Contador Pedal Derecho: 0");
+        temporizadorLabel = new JLabel("Tiempo: 0 segundos");
 
-        setLayout(new GridLayout(2, 2));
-        add(pedalIzquierdo);
-        add(pedalDerecho);
+        setLayout(new GridLayout(3, 2));
         add(indicador1);
         add(indicador2);
+        add(pedalIzquierdo);
+        add(pedalDerecho);
+        add(temporizadorLabel);
+        
 
-        pedalIzquierdo.addActionListener((ActionEvent e) -> {
-            pedalIzquierdo.setVisible(true);
-            pedalDerecho.setVisible(true);
-            // Actualizar propiedades de la bicicleta y los indicadores si es necesario
+        // Temporizador con intervalo de 1000 milisegundos (1 segundo)
+        tiempo = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                segundosTranscurridos++;
+                temporizadorLabel.setText("Tiempo: " + segundosTranscurridos + " segundos");
+                indicador1.setText("Contador Pedal Izquierdo: " + contadorPedalIzquierdo);
+                indicador2.setText("Contador Pedal Derecho: " + contadorPedalDerecho);
+            }
+        });
+        tiempo.start();
+
+        pedalIzquierdo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                contadorPedalIzquierdo++;
+                pedalIzquierdo.setVisible(true);
+                pedalDerecho.setVisible(true);
+            }
         });
 
-        pedalDerecho.addActionListener((ActionEvent e) -> {
-            pedalDerecho.setVisible(true);
-            pedalIzquierdo.setVisible(true);
-            // Actualizar propiedades de la bicicleta y los indicadores si es necesario
+        pedalDerecho.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                contadorPedalDerecho++;
+                pedalDerecho.setVisible(true);
+                pedalIzquierdo.setVisible(true);
+            }
         });
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new SimuladorBicicleta().setVisible(true);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SimuladorBicicleta().setVisible(true);
+            }
         });
     }
 }
